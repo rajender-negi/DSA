b677fdbc8a56d981f5439e6b689c37a530532bd5
rajender-negi <42839715+rajender-negi@users.noreply.github.com>
2021-12-08 13:27:42 +0530

    Update BST.java

--- BST.java
+++ BST.java
@@ -14,43 +14,26 @@ public class BST
 {
     Node root;
     
-    public static void searchElement(BST bst, int key)
+    public static void searchElement(BST bst, int key, String path)
     {
-        boolean gotIt = false;
-        String path= new String("path -> root -> ");
-        while(!gotIt)
+        if(bst.root==null || bst.root.data==key)
         {
-            System.out.println("start the search");
-            if(bst.root!=null)
-            {
-                System.out.println("root is not null");
-                System.out.println("value of bst's root data "+bst.root.data);
-                if(bst.root.data>key)
+            System.out.println("found the element :: root -> "+path);
+            return;
+        }else
+        {
+            if(bst.root.data>key)
                 {
-                    System.out.println("inside left child");
-                    path=path+(" left -> ");
+                    path=path+" left -> ";
                     bst.root=bst.root.left;
-                    if(bst.root.data==key)
-                    {
-                        gotIt = true;
-                        System.out.println("Found the element and path is :: "+path);
-                        return;
-                    }
+                    searchElement(bst, key, path);
                 }else
                 {
-                    System.out.println("inside right child");
-                    path=path+(" right -> ");
+                    path=path+" right -> ";
                     bst.root=bst.root.right;
-                    if(bst.root.data==key)
-                    {
-                        gotIt = true;
-                        System.out.println("Found the element and path is :: "+path);
-                        return;
-                    }        
+                    searchElement(bst, key, path);
                 }
-            }
         }
-        
     }
     
     public static void main(String args[])
@@ -66,6 +49,6 @@ public class BST
         bst.root.right.left = new Node(62);
         bst.root.right.right = new Node(100);
         
-        searchElement(bst, 37);
+        searchElement(bst, 37, "");
     }
 }
